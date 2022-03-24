4e2b430df964c1c6e1d7b7da3f8fc8a6ce363e00
dubtouch <postolachedan92@gmail.com>
2022-03-24 10:47:01 +0200

    initialize springboot project

---
+++ src/main/java/com/example/demo_extreme_sports/model/Country.java
@@ -0,0 +1,4 @@
+package com.example.demo_extreme_sports.model;
+
+public class Country {
+}
---
+++ src/test/java/com/example/demo_extreme_sports/DemoExtremeSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_extreme_sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExtremeSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
